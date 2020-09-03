cff76539e8dcd32fd94382580978aae09b99b76c
yanhailin666 <2560078008@qq.com>
2020-09-03 18:14:54 +0800

    代码管理

--- phone_backstage/urls.py
+++ phone_backstage/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('index',information)
+    path('index',dx),
+    path('home',home)
 ]
--- phone_backstage/views.py
+++ phone_backstage/views.py
@@ -1,18 +1,28 @@
-from django.http import HttpResponse
+from django.shortcuts import render,HttpResponse
+from django.http import JsonResponse
 import urllib
 from bs4 import BeautifulSoup
+from .models import *
 
 import logging
 logger = logging.getLogger("console")#配置日志
 
 
 def home(request):
-    
-    return HttpResponse()
+    id = request.GET.get('id')
+    dict = phone_test.objects.values().all()
+    #print(dict)
+    country_list=[]
+    phone_list=[]
+    information_list=[]
+    for data in dict:
+        #print(data)
+        country_list.append(data["country"])
+        phone_list.append(data["phone"])
+        information_list.append(data["information_url"])
+    return JsonResponse(information_list, safe=False)
 
-
-
-def information(request):
+def dx(request):
     url = 'http://www.z-sms.com/lv.php?pho_num=17194242613&1'
     page = urllib.request.urlopen(url)
     html = page.read().decode('utf-8')
